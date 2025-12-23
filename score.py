a5d2aae62459fafdc9c74b5b2416bcad9cf38fff
Allu Ketan Satya Sai <allusai2007@gmail.com>
2025-12-24 02:44:51 +0530

    Add Score class with score updating methods
    Implement Score class for tracking game scores

--- score.py
+++ score.py
@@ -1,1 +1,27 @@
+from turtle import Turtle
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.r_score = 0
+        self.l_score = 0
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Verdana", 60, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Verdana", 60, "normal"))
+
+    def inc_l_score(self):
+        self.l_score += 1
+        self.update_score()
+
+    def inc_r_score(self):
+        self.r_score += 1
+        self.update_score()
 
